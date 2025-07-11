0183e95277837a2b08e753ff54d46756df9d5eca
Jerry Bruwes <jbruwes@icloud.com>
2025-07-11 17:05:32 +0200

    modified:   src/modules.d.ts

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,3 +1,5 @@
 declare module "@quasar/quasar-ui-qiconpicker/src/components/icon-set/mdi-v6";
 declare module "vue-sfc-descriptor-to-string";
 declare module "monaco-editor/esm/vs/editor/editor.worker";
+declare module "jsonfeed-to-atom";
+declare module "jsonfeed-to-rss";
