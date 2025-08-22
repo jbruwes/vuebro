1e97ca6c2dc4d612f0112291943e657c33e4b4e0
Jerry Bruwes <jbruwes@icloud.com>
2025-08-22 18:48:01 +0200

    modified:   src/router/index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,6 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import routes from "./routes";
 
-/* -------------------------------------------------------------------------- */
-
 export default defineRouter(() => {
   const history = createMemoryHistory(process.env.VUE_ROUTER_BASE);
   return createRouter({ history, routes });
