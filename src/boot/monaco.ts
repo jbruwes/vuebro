fa162701a3516fbc619aa95eec5135ff66e54f6e
Jerry Bruwes <jbruwes@icloud.com>
2025-10-03 14:01:56 +0200

    modified:   src/boot/monaco.ts

--- src/boot/monaco.ts
+++ src/boot/monaco.ts
@@ -13,7 +13,14 @@ import {
   defaultLanguageSelector,
 } from "monaco-tailwind";
 import TailwindWorker from "monaco-tailwind/tailwind.worker?worker";
-import { highlighter } from "src/stores/app";
+import { createHighlighterCoreSync } from "shiki/core";
+import langJson from "shiki/langs/json.mjs";
+import langJsx from "shiki/langs/jsx.mjs";
+import langTsx from "shiki/langs/tsx.mjs";
+import langVue from "shiki/langs/vue.mjs";
+import themeDark from "shiki/themes/dark-plus.mjs";
+import themeLight from "shiki/themes/light-plus.mjs";
+import { engine } from "src/stores/app";
 
 window.MonacoEnvironment = {
   getWorker: (workerId: string, label: string) => {
@@ -35,4 +42,11 @@ configureMonacoTailwindcss(monaco, {
   languageSelector: [...defaultLanguageSelector, "vue"],
 });
 
-shikiToMonaco(highlighter, monaco as typeof monacoNs);
+shikiToMonaco(
+  createHighlighterCoreSync({
+    engine,
+    langs: [langVue, langJson, langJsx, langTsx],
+    themes: [themeDark, themeLight],
+  }),
+  monaco as typeof monacoNs,
+);
