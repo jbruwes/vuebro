4503d21e32c74279446b7354d16645e710725c02
Jerry Bruwes <jbruwes@icloud.com>
2025-10-16 14:10:48 +0200

    refactor eslint config for better performance

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,49 +1,37 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import type { ConfigWithExtendsArray } from "@eslint/config-helpers";
+
 import eslint from "@eslint/js";
+import gitignore from "eslint-config-flat-gitignore";
 import { flatConfigs } from "eslint-plugin-import-x";
 import perfectionist from "eslint-plugin-perfectionist";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import pluginVue from "eslint-plugin-vue";
+import vuePug from "eslint-plugin-vue-pug";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
 import { configs, parser } from "typescript-eslint";
 import vueParser from "vue-eslint-parser";
 
-const compat = new FlatCompat();
 export default defineConfig(
-  { ignores: ["**/dist", "**/.quasar", "**/language-configs.ts"] },
-  {
-    rules: {
-      "@typescript-eslint/no-use-before-define": ["error", "nofunc"],
-      "import-x/no-extraneous-dependencies": [
-        "error",
-        {
-          devDependencies: ["**/quasar.config.ts", "**/eslint.config.ts"],
-          optionalDependencies: false,
-          whitelist: ["electron"],
-        },
-      ],
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
-    },
-  },
+  gitignore(),
   {
     languageOptions: {
+      globals: globals.browser,
       parser: vueParser,
       parserOptions: {
         extraFileExtensions: [".vue"],
         parser,
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
   },
-  ...compat.extends("plugin:vue-pug/vue3-recommended"),
+  vuePug.configs["flat/recommended"] as ConfigWithExtendsArray,
   eslint.configs.recommended,
-  //@ts-expect-error Argument of type 'PluginFlatConfig' is not assignable to parameter of type 'InfiniteArray<ConfigWithExtends>'.
-  flatConfigs.recommended,
-  flatConfigs.typescript,
+  flatConfigs.recommended as ConfigWithExtendsArray,
+  flatConfigs.typescript as ConfigWithExtendsArray,
   configs.strictTypeChecked,
   configs.stylisticTypeChecked,
-  ...pluginVue.configs["flat/recommended"],
+  pluginVue.configs["flat/recommended"],
   perfectionist.configs["recommended-natural"],
   eslintPluginPrettierRecommended,
 );
