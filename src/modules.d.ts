30be56f6ddd2515fff197b8bf752e711e09a81a3
Jerry Bruwes <jbruwes@icloud.com>
2025-10-16 14:10:24 +0200

    Add ESLint plugin module declaration

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,5 +1,7 @@
 declare module "@quasar/quasar-ui-qiconpicker/src/components/icon-set/mdi-v6";
 declare module "vue-sfc-descriptor-to-string";
-declare module "monaco-editor/esm/vs/editor/editor.worker";
 declare module "jsonfeed-to-atom";
 declare module "jsonfeed-to-rss";
+declare module "eslint-plugin-vue-pug" {
+  export const configs: Record<string, unknown>;
+}
