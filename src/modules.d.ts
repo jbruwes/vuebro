76ae93e3206a4f5fc541dcad3dab5be41dc51d5f
Jerry Bruwes <jbruwes@icloud.com>
2025-10-22 17:23:40 +0200

    Remove unused TypeScript module declarations

--- src/modules.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "@quasar/quasar-ui-qiconpicker/src/components/icon-set/mdi-v6";
-declare module "vue-sfc-descriptor-to-string";
-declare module "jsonfeed-to-atom";
-declare module "jsonfeed-to-rss";
-declare module "eslint-plugin-vue-pug" {
-  export const configs: Record<string, unknown>;
-}
