513541eaba27301bf9873458e60cb0e206a19648
Jerry Bruwes <jbruwes@icloud.com>
2025-10-22 17:06:17 +0200

    refactor: replace custom eslint config with shared config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,37 +1,10 @@
-import type { ConfigWithExtendsArray } from "@eslint/config-helpers";
-
-import eslint from "@eslint/js";
-import gitignore from "eslint-config-flat-gitignore";
-import { flatConfigs } from "eslint-plugin-import-x";
-import perfectionist from "eslint-plugin-perfectionist";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
-import pluginVue from "eslint-plugin-vue";
-import vuePug from "eslint-plugin-vue-pug";
+import shared from "@vuebro/configs/eslint";
 import { defineConfig } from "eslint/config";
-import globals from "globals";
-import { configs, parser } from "typescript-eslint";
-import vueParser from "vue-eslint-parser";
 
-export default defineConfig(
-  gitignore(),
-  {
-    languageOptions: {
-      globals: globals.browser,
-      parser: vueParser,
-      parserOptions: {
-        extraFileExtensions: [".vue"],
-        parser,
-        projectService: true,
-      },
+export default defineConfig(shared, {
+  languageOptions: {
+    parserOptions: {
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-  vuePug.configs["flat/recommended"] as ConfigWithExtendsArray,
-  eslint.configs.recommended,
-  flatConfigs.recommended as ConfigWithExtendsArray,
-  flatConfigs.typescript as ConfigWithExtendsArray,
-  configs.strictTypeChecked,
-  configs.stylisticTypeChecked,
-  pluginVue.configs["flat/recommended"],
-  perfectionist.configs["recommended-natural"],
-  eslintPluginPrettierRecommended,
-);
+});
