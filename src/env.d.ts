b6387b7de40272197a973c88c23deb92c17267e1
Jerry Bruwes <jbruwes@icloud.com>
2025-10-22 17:23:35 +0200

    Add type declarations for Quasar and conversion utilities

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,16 @@
-/* eslint-disable */
-
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: string;
-    VUE_ROUTER_MODE: "hash" | "history" | "abstract" | undefined;
     VUE_ROUTER_BASE: string | undefined;
+    VUE_ROUTER_MODE: "abstract" | "hash" | "history" | undefined;
     // Define any custom env variables you have here, if you wish
   }
 }
-
 declare const __APP_VERSION__: string;
+declare module "@quasar/quasar-ui-qiconpicker/src/components/icon-set/mdi-v6";
+declare module "vue-sfc-descriptor-to-string";
+declare module "jsonfeed-to-atom";
+declare module "jsonfeed-to-rss";
+declare module "eslint-plugin-vue-pug" {
+  export const configs: Record<string, unknown>;
+}
